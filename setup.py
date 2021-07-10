36e856844c1c581c85387946684361f2c7e2c9be
Carlos Cesar Caballero <ccesar@daxslab.com>
2021-07-10 15:13:17 -0400

    add console script to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='pytodusdownload',
     version='0.1',
     packages=['pytodusdownload'],
-    scripts=['pytodusdownload/pytodusdownload.py'],
+    entry_points = {
+            'console_scripts': ['pytodusdownload=pytodusdownload.pytodusdownload:main']
+    },
     url='https://github.com/cccaballero/pytodusdownload',
     license='AGPL',
     author='Carlos Cesar Caballero Díaz',
